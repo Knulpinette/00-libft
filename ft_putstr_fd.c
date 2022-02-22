9d309d08b5b8f2e02ea78cc030f98c1570e59853
Knulpinette <ophelie.surcouf@gmail.com>
2022-02-22 11:56:32 +0100

    made putstr more efficient

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -22,14 +22,10 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	int	i;
+	size_t	s_len;
 
 	if (!s)
 		return ;
-	i = 0;
-	while (s[i] != '\0')
-	{
-		write(fd, &s[i], 1);
-		i++;
-	}
+	s_len = ft_strlen(s);
+	write(fd, s, s_len);
 }
